9d38f66f847c9a2cb7b03a2a4e2902836e517723
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2026-01-24 09:45:23 +0100

    Add info to auth store

--- 01-react-foundation/src/store/auth.store.ts
+++ 01-react-foundation/src/store/auth.store.ts
@@ -11,6 +11,7 @@ interface AuthState {
 }
 // Memory space to save the state, like a custom hook which saves the state
 // (set) defines new state
+// This sets up a global state, with Zustand we don't need to provide it at components
 export const useAuthStore = create<AuthState>()( (set)=> ({
     status: 'checking',
     token: undefined,
