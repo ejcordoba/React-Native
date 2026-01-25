a01c7ceeda3a56b5b933b0defd9c67e3bbf6e590
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2026-01-25 16:28:25 +0100

    Set fetch data at state and use it to render data

--- 01-react-foundation/src/components/UsersPage.tsx
+++ 01-react-foundation/src/components/UsersPage.tsx
@@ -1,10 +1,10 @@
 import axios from "axios"
-import { useEffect } from "react"
-import type { ReqResUserListResponse } from "../interfaces"
-
-const loadUsers = async() => {
+import { useEffect, useState } from "react"
+import type { ReqResUserListResponse, User } from "../interfaces"
+// loadUsers awaits a type User from the interface ReqResUserListResponse
+const loadUsers = async(): Promise<User[]> => {
     try {
-        const { data } = await axios.get<ReqResUserListResponse>('https://reqres.in/api/users,',{
+        const { data } = await axios.get<ReqResUserListResponse>('https://reqres.in/api/users',{
         headers: {
           "x-api-key": import.meta.env.VITE_REQRES_API_KEY,
         },
@@ -19,9 +19,14 @@ const loadUsers = async() => {
 export const UsersPage = () => {
     // Data will trigger twice due to <React.StrictMode></React.StrictMode> at Main.tsx
     // To assure side-effect doesn't triggers any issue at the app
+
+    // useState allows us to refresh the component when the state changes
+    const [users, setUsers] = useState<User[]>([]);
     useEffect(() => {
 
-       loadUsers().then(users => console.log(users));
+       loadUsers()
+        .then(users => setUsers(users));
+    //.then(setUsers) <--- we can do it this way, sending the function, then it set all the arguments in 
 
     }, [])
     return (
@@ -36,11 +41,15 @@ export const UsersPage = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>avatar</td>
-                        <td>nombre</td>
-                        <td>email</td>
-                    </tr>
+                    {
+                        users.map( user => (
+                            <tr key={user.id}>
+                                <td><img style={{width:'50px'}} src={user.avatar} alt="User avatar"/></td>
+                                <td>{user.first_name} {user.last_name}</td>
+                                <td>{user.email}</td>
+                            </tr>
+                        ))
+                    }
                 </tbody>
             </table>
         </>
