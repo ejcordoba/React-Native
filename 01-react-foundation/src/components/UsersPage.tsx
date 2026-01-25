b693218eb80e035e1227f0cfadec1fdb10ed54af
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2026-01-25 17:22:51 +0100

    Set simple pagination to users page

--- 01-react-foundation/src/components/UsersPage.tsx
+++ 01-react-foundation/src/components/UsersPage.tsx
@@ -1,10 +1,13 @@
 import axios from "axios"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import type { ReqResUserListResponse, User } from "../interfaces"
 // loadUsers awaits a type User from the interface ReqResUserListResponse
-const loadUsers = async(): Promise<User[]> => {
+const loadUsers = async(page:number = 1): Promise<User[]> => {
     try {
         const { data } = await axios.get<ReqResUserListResponse>('https://reqres.in/api/users',{
+        params:{
+            page:page
+        },
         headers: {
           "x-api-key": import.meta.env.VITE_REQRES_API_KEY,
         },
@@ -22,13 +25,32 @@ export const UsersPage = () => {
 
     // useState allows us to refresh the component when the state changes
     const [users, setUsers] = useState<User[]>([]);
+    const currentPageRef = useRef(1);
     useEffect(() => {
 
-       loadUsers()
+       loadUsers(currentPageRef.current)
         .then(users => setUsers(users));
     //.then(setUsers) <--- we can do it this way, sending the function, then it set all the arguments in 
 
-    }, [])
+    }, []);
+    
+    const nextPage = async() => {
+        currentPageRef.current++;
+        const users = await loadUsers(currentPageRef.current);
+        if ( users.length > 0) {
+            setUsers(users);
+        } else {
+            currentPageRef.current--;
+        }
+    }
+    const prevPage = async() => {
+        if (currentPageRef.current < 1) return;
+        currentPageRef.current--;
+        const users = await loadUsers(currentPageRef.current);
+        setUsers(users);
+    }
+
+ 
     return (
         <>
             <h3>Usuarios:</h3>
@@ -48,6 +70,10 @@ export const UsersPage = () => {
                     }
                 </tbody>
             </table>
+            <div>
+                <button onClick={prevPage}>Prev</button>
+                <button onClick={nextPage}>Next</button>
+            </div>
         </>
     )
 }
