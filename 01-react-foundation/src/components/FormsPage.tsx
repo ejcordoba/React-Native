2292c30e8e673b6c1f3797c1553f194278be5765
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2026-01-30 20:10:29 +0100

    Some examples of React Hook Form functions

--- 01-react-foundation/src/components/FormsPage.tsx
+++ 01-react-foundation/src/components/FormsPage.tsx
@@ -5,22 +5,31 @@ type FormInputs = {
     password: string;
 }
 export const FormsPage = () => {
-    const { register } = useForm<FormInputs>({
+    const { register , handleSubmit, formState, watch } = useForm<FormInputs>({
         defaultValues: {
             email: 'eduardo@google.com',
             password: 'Abc123'
         }
     });
 
+    const onSubmit = (myForm: FormInputs) => {
+        console.log({myForm})
+    }
+    console.log(watch('email'))
 
   return (
     <>
-    <h3>Formularios</h3>
-    <div style={{display:'flex', flexDirection:'column'}}>
-        <input type="text" placeholder="Email" {...register('email')}/>
-        <input type="text" placeholder="Password" {...register('password')}/>
-        <button type="submit">Ingresar</button>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+        <h3>Formularios</h3>
+        <div style={{display:'flex', flexDirection:'column'}}>
+            <input type="text" placeholder="Email" {...register('email', {required: true})}/>
+            <input type="text" placeholder="Password" {...register('password', {required: true})}/>
+            <button type="submit">Ingresar</button>
+        </div>
+    </form>
+    <pre>
+        {JSON.stringify(formState, null, 2)}
+    </pre>
     </>
   )
 }
