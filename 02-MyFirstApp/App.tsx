e493141227b6340ee6da6d4ffa7731046ba88746
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2026-02-01 11:52:06 +0100

    add counter component, set usestate and use pressable component to handle function

--- 02-MyFirstApp/App.tsx
+++ 02-MyFirstApp/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { HelloWorldScreen } from './src/presentation/screens/HelloWorldScreen'
+import { CounterScreen } from './src/presentation/screens/CounterScreen'
+import { StyleSheet } from 'react-native'
 
 export const App = () => {
   return (
     <SafeAreaView style={{flex:1}}>
-        <HelloWorldScreen name='Eduardo Córdoba'/>
+        {/* <HelloWorldScreen name='Eduardo Córdoba'/> */}
+        <CounterScreen/>
     </SafeAreaView>
   )
 }
---
+++ 02-MyFirstApp/src/presentation/screens/CounterScreen.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { Text, View, StyleSheet,Pressable } from 'react-native';
+
+
+export const CounterScreen = () => {
+
+  const [count, setCount] = useState(10)
+  return (
+    <View style={styles.container}>
+        <Text style={styles.title}>{count}</Text>
+        <Pressable 
+            onPress={()=>{setCount(count + 1)}}
+            onLongPress={()=>{setCount(0)}}
+        >
+            <Text>+1</Text>
+        </Pressable>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    title: {
+        fontSize:80,
+        color: 'black',
+        fontWeight: '300'
+    }
+})
