c4880fc739e0a621646e1fb3dc417b7402b6300a
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2026-02-01 17:13:07 +0100

    Use React Native Paper FAB 'button'

--- 02-MyFirstApp/App.tsx
+++ 02-MyFirstApp/App.tsx
@@ -4,13 +4,15 @@ import { HelloWorldScreen } from './src/presentation/screens/HelloWorldScreen'
 import { CounterScreen } from './src/presentation/screens/CounterScreen'
 import { StyleSheet } from 'react-native'
 import { PaperProvider } from 'react-native-paper'
+import { CounterM3Screen } from './src/presentation/screens/CounterM3';
 
 export const App = () => {
   return (
     <PaperProvider>
         <SafeAreaView style={{flex:1}}>
             {/* <HelloWorldScreen name='Eduardo Córdoba'/> */}
-            <CounterScreen/>
+            {/* <CounterScreen/> */}
+            <CounterM3Screen/>
         </SafeAreaView>
     </PaperProvider>
   )
---
+++ 02-MyFirstApp/src/presentation/theme/global.styles.ts
@@ -0,0 +1,27 @@
+import { Platform, StyleSheet } from "react-native";
+
+
+
+
+
+export const globalStyles = StyleSheet.create({
+
+    centerContainer: {
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 80,
+        fontWeight: '300',
+        color: 'black'
+    },
+     fab: {
+        position: 'absolute',
+        margin: 16,
+        right: 5,
+        bottom: Platform.OS === 'android' ? 10 : 0,
+    },
+
+
+})
